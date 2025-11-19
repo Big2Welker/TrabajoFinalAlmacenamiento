4af850cad2a451c23fe310238d09ad29c02e96aa
Big2Welker <sebastian.rojas_h@uao.edu.co>
2025-11-18 22:52:08 -0500

    Delete SistemaAcademico - copia directory

--- SistemaAcademico - copia/app/api/v1/api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1.routes import (
-    evento_routes,
-    evaluacion_routes  
-)
-api_router_v1 = APIRouter()
-
-api_router_v1.include_router(evento_routes.router, prefix="/eventos", tags=["Eventos"])
-api_router_v1.include_router(evaluacion_routes.router, prefix="/evaluaciones", tags=["Evaluaciones"])
